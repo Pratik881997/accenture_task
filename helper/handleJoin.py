8fc61691847ff026b6cd95bf1f92e7415d43f9ab
technologire <technologire@gmail.com>
2023-06-05 00:05:30 +0530

    added try except block to handle un handled exceptions

--- helper/handleJoin.py
+++ helper/handleJoin.py
@@ -31,19 +31,22 @@ class HandleJoin:
 
     # possible types leftJoin, innerJoin, fullOuterJoin, rightJoin
     def dataframe_join(self):
-        if self._validate_join_type():
-            if len(self.join_keys) >= 1:
-                left_keys, right_keys = self._get_lkeys_rkeys()
+        try:
+            if self._validate_join_type():
+                if len(self.join_keys) >= 1:
+                    left_keys, right_keys = self._get_lkeys_rkeys()
 
-                df_joined = self.left_dataframe.merge(self.right_dataframe,
-                                                      left_on=left_keys,
-                                                      right_on= right_keys,
-                                                      how=self.possible_joins[self.join_type])
+                    df_joined = self.left_dataframe.merge(self.right_dataframe,
+                                                          left_on=left_keys,
+                                                          right_on= right_keys,
+                                                          how=self.possible_joins[self.join_type])
 
-                df_joined_dict = df_joined.to_dict()
-                return jsonify(data=df_joined_dict), 200
-
+                    df_joined_dict = df_joined.to_dict()
+                    return jsonify(output={"data":df_joined_dict}), 200
+                else:
+                    return jsonify(joinTypeError="please pass atleast 1 join key"), 401
             else:
-                return jsonify(joinTypeError="please pass atleast 1 join key"), 401
-        else:
-            return jsonify(joinTypeError="Invalid Join Type, please choose valid Join type"), 401
+                return jsonify(joinTypeError="Invalid Join Type, please choose valid Join type"), 401
+
+        except Exception as e:
+            return jsonify(joinTypeError="contact admin with this error: " + str(e)), 401
