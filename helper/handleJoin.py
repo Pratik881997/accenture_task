e4b103e15ce4611fd4cc9f8d1c9fad6c5eef4fec
technologire <technologire@gmail.com>
2023-06-05 12:40:17 +0530

    Added comments in code updated response messages and updated body example

--- helper/handleJoin.py
+++ helper/handleJoin.py
@@ -3,6 +3,7 @@ from flask import jsonify
 
 
 class HandleJoin:
+    # Constructor to initialized values on the time of object creation
     def __init__(self, left_dataframe_data, right_dataframe_data, join_type, join_key):
         self.left_dataframe = pd.DataFrame(left_dataframe_data['data'])
         self.right_dataframe = pd.DataFrame(right_dataframe_data['data'])
@@ -15,12 +16,14 @@ class HandleJoin:
                                'crossJoin': 'cross'
                                }
 
+    # Protected Private method to validate join type
     def _validate_join_type(self):
         if self.join_type in self.possible_joins.keys():
             return True
         else:
             return False
 
+    # Protected Private method to get Keys from dictionary passed in request body
     def _get_lkeys_rkeys(self):
         l_keys = []
         r_keys = []
@@ -29,24 +32,32 @@ class HandleJoin:
             r_keys.append(keys_data['rightKey'])
         return l_keys, r_keys
 
-    # possible types leftJoin, innerJoin, fullOuterJoin, rightJoin
+    # pubic method to Validate data and return joined dataframe according to passed values
     def dataframe_join(self):
         try:
             if self._validate_join_type():
                 if len(self.join_keys) >= 1:
                     left_keys, right_keys = self._get_lkeys_rkeys()
 
-                    df_joined = self.left_dataframe.merge(self.right_dataframe,
-                                                          left_on=left_keys,
-                                                          right_on= right_keys,
-                                                          how=self.possible_joins[self.join_type])
+                    validate_left_keys = set(left_keys).issubset(set(self.left_dataframe.columns))
+                    validate_right_keys = set(right_keys).issubset(set(self.right_dataframe.columns))
 
-                    df_joined_dict = df_joined.to_dict()
-                    return jsonify(output={"data":df_joined_dict}), 200
+                    print(self.possible_joins[self.join_type])
+                    if validate_left_keys and validate_right_keys:
+                        df_joined = self.left_dataframe.merge(self.right_dataframe,
+                                                              left_on=left_keys,
+                                                              right_on=right_keys,
+                                                              how=self.possible_joins[self.join_type])
+
+                        df_joined_dict = df_joined.to_dict(orient='records')
+                        return jsonify(output={"data": df_joined_dict}), 200
+                    else:
+                        return jsonify(InvalidKey="Please pass a Key that available in dataframe"), 401
                 else:
-                    return jsonify(joinTypeError="please pass atleast 1 join key"), 401
+                    return jsonify(JoinKeyRequired="please pass atleast 1 join key"), 401
             else:
-                return jsonify(joinTypeError="Invalid Join Type, please choose valid Join type"), 401
+                return jsonify(joinTypeError="Invalid Join Type, please choose valid Join type. valid join types are " + str(list(self.possible_joins.keys()))), 401
 
+        # handling runtime error that did not handle in above code
         except Exception as e:
-            return jsonify(joinTypeError="contact admin with this error: " + str(e)), 401
+            return jsonify(Error="contact admin with this error: " + str(e)), 401
